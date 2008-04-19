ee3607587a8b47c46a70e6423f419951ca68e73d
plinehan <plinehan@bed60e23-ce3b-0410-a9c8-4181b64b1b3e>
2008-04-19 22:21:58 +0000

    initial port to appengine.

---
+++ www/trunk/plinehandotcom.py
@@ -0,0 +1,81 @@
+import wsgiref.handlers
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write(template.render('index.html', {}))
+
+class OfCourse(webapp.RequestHandler):
+  def get(self):
+    values = {
+      'bgcolor':  '#FFFFFF',
+      'text':     '#000000',
+      'next_url': '/',
+      'image':    'ofcourse.jpg'
+      }
+    self.response.out.write(template.render('template.html', values))
+
+class FunnyMan(webapp.RequestHandler):
+  def get(self):
+    values = {
+      'bgcolor':  '#000000',
+      'text':     '#FFFFFF',
+      'next_url': 'brown',
+      'image':    'funnyman.jpg'
+      }
+    self.response.out.write(template.render('template.html', values))
+
+class Brown(webapp.RequestHandler):
+  def get(self):
+    values = {
+      'bgcolor':  '#000000',
+      'text':     '#FFFFFF',
+      'next_url': 'nurbs',
+      'image':    'brown.jpg'
+      }
+    self.response.out.write(template.render('template.html', values))
+
+class Nurbs(webapp.RequestHandler):
+  def get(self):
+    values = {
+      'bgcolor':  '#FFFFFF',
+      'text':     '#000000',
+      'next_url': 'thenextlevel',
+      'image':    'nurbs.jpg'
+      }
+    self.response.out.write(template.render('template.html', values))
+
+class TheNextLevel(webapp.RequestHandler):
+  def get(self):
+    values = {
+      'bgcolor':  '#FFFFFF',
+      'text':     '#000000',
+      'next_url': 'dog',
+      'image':    'thenextlevel.jpg'
+      }
+    self.response.out.write(template.render('template.html', values))
+
+class Dog(webapp.RequestHandler):
+  def get(self):
+    values = {
+      'bgcolor':  '#000000',
+      'text':     '#FFFFFF',
+      'next_url': 'http://www.johnniemanzari.com',
+      'image':    'dog.jpg'
+      }
+    self.response.out.write(template.render('template.html', values))
+    
+def main():
+  application = webapp.WSGIApplication([('/', MainPage),
+                                        ('/ofcourse', OfCourse),
+                                        ('/funnyman', FunnyMan),
+                                        ('/brown', Brown),
+                                        ('/nurbs', Nurbs),
+                                        ('/thenextlevel', TheNextLevel),
+                                        ('/dog', Dog)],
+                                       debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == '__main__':
+  main()
